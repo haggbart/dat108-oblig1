3000b9751c68d3a58ed901971997a658234c51be
160910 <54103520+160910@users.noreply.github.com>
2020-09-11 11:03:03 +0200

    Create Main.java

---
+++ src/main/java/no/hvl/dat108/oblig1/oppgave2/Main.java
@@ -0,0 +1,20 @@
+package no.hvl.dat108.oblig1.oppgave2;
+
+public class Main {
+    public static void main(String[] args) {
+        Rutsjebane bane = new Rutsjebane();
+
+        Kokk kokk1 = new Kokk(bane,"Kokk1");
+        Kokk kokk2 = new Kokk(bane,"Kokk2");
+        Kokk kokk3 = new Kokk(bane,"Kokk3");
+
+        Servitor servitor1 = new Servitor(bane, "Servitor1");
+        Servitor servitor2 = new Servitor(bane, "Servitor2");
+
+        kokk1.start();
+        kokk2.start();
+        kokk3.start();
+        servitor1.start();
+        servitor2.start();
+    }
+}
